5b59bf042c3c93b5dc465de23abb299b3b3a0181
JEKrieger <joaoeduardokrieger123@gmail.com>
2023-06-11 20:48:44 -0300

    Adição do FontAwesome Icons

--- src/components/AdminProductTable/AdminProductTable.js
+++ src/components/AdminProductTable/AdminProductTable.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faSquarePlus } from "@fortawesome/free-solid-svg-icons";
 
 export default function AdminProductTable (){
   const [registros, setRegistros] = useState([]);
@@ -18,7 +22,7 @@ export default function AdminProductTable (){
     <div id="admin-product" >
         <div className="d-flex justify-content-center mt-5">
             <div className="d-flex justify-content-end w-75">
-                <button className="btn btn-custom px-4 text-white">Incluir</button>
+                <button className="btn btn-custom px-4 text-white"><FontAwesomeIcon icon={faSquarePlus} style={{color: "#ffffff",}}/>&nbsp;Incluir</button>
             </div>
         </div>
         <div className="d-flex justify-content-center mt-2">
@@ -44,8 +48,8 @@ export default function AdminProductTable (){
                         <td>{registro.quantidade}</td>
                         <td>{registro.categoria.nome}</td>
                         <td>{registro.fornecedor.nome}</td>
-                        <td className="text-center"><button className="btn btn-warning px-4 text-white">Editar</button></td>
-                        <td className="text-center"><button className="btn btn-danger px-4 text-white">Excluir</button></td>
+                        <td className="text-center"><button className="btn btn-warning px-4 text-white"><FontAwesomeIcon icon={faPenToSquare} style={{color: "#000000",}} /></button></td>
+                        <td className="text-center"><button className="btn btn-danger px-4 text-white"><FontAwesomeIcon icon={faTrash} style={{color: "#000000",}} /></button></td>
                     </tr>
                     ))}
                 </tbody>
